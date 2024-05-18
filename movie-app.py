ae5cfd59cdf686b4e32358228309fad33dcc2156
utkarshmisra0211 <utkarsh.misra2000@gmail.com>
2024-05-18 22:57:15 +0530

    debugging

---
+++ movie-app.py
@@ -0,0 +1,62 @@
+import streamlit as st
+import pickle
+import pandas as pd
+import requests
+import time
+from requests.exceptions import RequestException
+
+st.set_page_config(layout="wide")
+def fetch_poster(movie_id):
+    max_retries = 3
+    retry_delay = 1  # in seconds
+    for _ in range(max_retries):
+        try:
+            response = requests.get("https://api.themoviedb.org/3/movie/{}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US".format(movie_id))
+            response.raise_for_status()
+            data = response.json()
+            return "http://image.tmdb.org/t/p/w500/" + data['poster_path']
+        except RequestException as e:
+            st.warning(f"Failed to fetch poster. Retrying in {retry_delay} seconds...")
+            time.sleep(retry_delay)
+    st.error("Failed to fetch poster after multiple attempts. Please try again later.")
+    return None
+
+def recommend(movie):
+    movie_index = movies[movies['title'] == movie].index[0]
+    distances = similarity[movie_index]
+    movies_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[1:6]
+    recommended_movies = []
+    recommended_poster_urls = []
+    for i in movies_list:
+        movie_id = movies.iloc[i[0]].movie_id
+        recommended_movies.append(movies.iloc[i[0]].title)
+        poster_url = fetch_poster(movie_id)
+        if poster_url:
+            recommended_poster_urls.append(poster_url)
+    return recommended_movies, recommended_poster_urls
+
+st.title('Movie Recommender')
+similarity = pickle.load(open('similarity.pkl', 'rb'))
+
+# Load movie dictionary
+movie_dict = pickle.load(open('movies.pkl', 'rb'))
+movies = pd.DataFrame(movie_dict)
+
+# Select box with a default non-selectable option
+selected_movie_name = st.selectbox(
+    "Find Movie",
+    ["Select movie"] + list(movies['title'].values),
+    index=0,
+    format_func=lambda x: "Select a movie" if x == "Select movie" else x
+)
+
+# To Check if the selected option is valid
+if selected_movie_name != "Select movie":
+    if st.button("Recommend"):
+        names, posters = recommend(selected_movie_name)
+        columns = st.columns(len(names))
+        for name, poster_url, col in zip(names, posters, columns):
+            col.header(name)
+            col.image(poster_url)
+else:
+    st.write("Please select a movie from the dropdown.")
