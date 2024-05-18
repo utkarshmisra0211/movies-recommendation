b0a08f1d5f44e3152ff656c2dbc72e7c21e3c785
utkarshmisra0211 <utkarsh.misra2000@gmail.com>
2024-05-18 23:09:24 +0530

    finalizing streamlit

--- movie-app.py
+++ movie-app.py
@@ -6,19 +6,18 @@ import time
 from requests.exceptions import RequestException
 
 st.set_page_config(layout="wide")
+
 def fetch_poster(movie_id):
     max_retries = 3
     retry_delay = 1  # in seconds
     for _ in range(max_retries):
         try:
-            response = requests.get("https://api.themoviedb.org/3/movie/{}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US".format(movie_id))
+            response = requests.get(f"https://api.themoviedb.org/3/movie/{movie_id}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US")
             response.raise_for_status()
             data = response.json()
             return "http://image.tmdb.org/t/p/w500/" + data['poster_path']
-        except RequestException as e:
-            st.warning(f"Failed to fetch poster. Retrying in {retry_delay} seconds...")
+        except RequestException:
             time.sleep(retry_delay)
-    st.error("Failed to fetch poster after multiple attempts. Please try again later.")
     return None
 
 def recommend(movie):
@@ -31,8 +30,7 @@ def recommend(movie):
         movie_id = movies.iloc[i[0]].movie_id
         recommended_movies.append(movies.iloc[i[0]].title)
         poster_url = fetch_poster(movie_id)
-        if poster_url:
-            recommended_poster_urls.append(poster_url)
+        recommended_poster_urls.append(poster_url)
     return recommended_movies, recommended_poster_urls
 
 st.title('Movie Recommender')
@@ -50,13 +48,16 @@ selected_movie_name = st.selectbox(
     format_func=lambda x: "Select a movie" if x == "Select movie" else x
 )
 
-# To Check if the selected option is valid
+# To check if the selected option is valid
 if selected_movie_name != "Select movie":
     if st.button("Recommend"):
         names, posters = recommend(selected_movie_name)
         columns = st.columns(len(names))
         for name, poster_url, col in zip(names, posters, columns):
             col.header(name)
-            col.image(poster_url)
+            if poster_url:
+                col.image(poster_url)
+            else:
+                col.write("Poster not available")
 else:
     st.write("Please select a movie from the dropdown.")
